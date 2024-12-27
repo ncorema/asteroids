a01c85ee41780fb788cf537d007728fe43216c23
ncorema <kupsch.matthias@gmail.com>
2024-12-27 14:06:13 +0100

    created drawable and updateable groups

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     frameclock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,10 +22,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0,0,0))
-        player.draw(screen)
+        
+        for updateable_item in updateable:
+            updateable_item.update(dt)
+
+        for drawable_item in drawable:
+            drawable_item.draw(screen)
+
         pygame.display.flip()
         dt = frameclock.tick(60) / 1000
-        
+
 if __name__ == "__main__":
     main()
