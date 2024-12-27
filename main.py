3ae5e06211867475c11047204b64c1cbe49af957
ncorema <kupsch.matthias@gmail.com>
2024-12-27 12:13:29 +0100

    added player drawing logic

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    frameclock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -13,7 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = frameclock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
